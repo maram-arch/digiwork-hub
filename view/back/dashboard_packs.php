5dac3a9aa9ec281aaaab65d14c1c4db6e8a4f0d9
mariemhammami1A27 <mariem.hammami@esprit.tn>
2026-04-10 12:57:28 +0100

    add back office UI

---
+++ view/back/dashboard_packs.php
@@ -0,0 +1,27 @@
+﻿<?php
+require_once("../../model/Pack.php");
+$pack = new Pack();
+$packs = $pack->getAll();
+?>
+
+<h2>Gestion Packs</h2>
+
+<form action="../../controller/PackController.php" method="POST">
+<input type="text" name="nom" placeholder="Nom">
+<input type="number" name="prix" placeholder="Prix">
+<input type="date" name="duree">
+<textarea name="description"></textarea>
+<input type="number" name="nb">
+<select name="support">
+<option>oui</option>
+<option>non</option>
+</select>
+<button name="add">Ajouter</button>
+</form>
+
+<?php foreach($packs as $p): ?>
+<div>
+<?= $p['nom-pack'] ?> - <?= $p['prix'] ?>
+<a href="../../controller/PackController.php?delete=<?= $p['id-pack'] ?>">Supprimer</a>
+</div>
+<?php endforeach; ?>

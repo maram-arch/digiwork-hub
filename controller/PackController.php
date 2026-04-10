5a8d9b61b5134a024a8bca8b5051f9289b6b7fe6
mariemhammami1A27 <mariem.hammami@esprit.tn>
2026-04-10 12:54:33 +0100

    add pack controller logic

---
+++ controller/PackController.php
@@ -0,0 +1,22 @@
+﻿<?php
+require_once("../model/Pack.php");
+
+$pack = new Pack();
+
+if(isset($_POST['add'])) {
+    $pack->add(
+        $_POST['nom'],
+        $_POST['prix'],
+        $_POST['duree'],
+        $_POST['description'],
+        $_POST['nb'],
+        $_POST['support']
+    );
+    header("Location: ../view/back/dashboard_packs.php");
+}
+
+if(isset($_GET['delete'])) {
+    $pack->delete($_GET['delete']);
+    header("Location: ../view/back/dashboard_packs.php");
+}
+?>

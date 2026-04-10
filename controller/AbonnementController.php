ef3d74038436641c918a68c192c40f7982c94230
mariemhammami1A27 <mariem.hammami@esprit.tn>
2026-04-10 12:56:37 +0100

    add controllers

---
+++ controller/AbonnementController.php
@@ -0,0 +1,10 @@
+﻿<?php
+require_once("../model/Abonnement.php");
+
+$abonnement = new Abonnement();
+
+if(isset($_POST['subscribe'])) {
+    $abonnement->subscribe(1, $_POST['pack_id']);
+    header("Location: ../view/front/packs.php");
+}
+?>

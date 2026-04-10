4d538a87fa2813d6e6c25272a6ebf443f9c40d39
mariemhammami1A27 <mariem.hammami@esprit.tn>
2026-04-10 12:52:29 +0100

    add abonnement logic

---
+++ model/Abonnement.php
@@ -0,0 +1,26 @@
+﻿<?php
+require_once("../config/config.php");
+
+class Abonnement {
+
+    function subscribe($user, $pack) {
+        global $pdo;
+
+        $sql1 = "INSERT INTO `abonnement`
+        (`id-user`, `date-deb`, `date-fin`, `status`)
+        VALUES (?, NOW(), DATE_ADD(NOW(), INTERVAL 30 DAY), 'actif')";
+
+        $stmt1 = $pdo->prepare($sql1);
+        $stmt1->execute([$user]);
+
+        $id_abonnement = $pdo->lastInsertId();
+
+        $sql2 = "INSERT INTO `abon-pack`
+        (`id-pack`, `id-abonnement`)
+        VALUES (?, ?)";
+
+        $stmt2 = $pdo->prepare($sql2);
+        $stmt2->execute([$pack, $id_abonnement]);
+    }
+}
+?>

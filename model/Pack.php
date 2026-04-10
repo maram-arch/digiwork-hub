dbac72077f88139fef41cc3cfa152e0ba92271c9
mariemhammami1A27 <mariem.hammami@esprit.tn>
2026-04-10 12:45:46 +0100

    update Pack model with CRUD methods

---
+++ model/Pack.php
@@ -0,0 +1,28 @@
+﻿<?php
+require_once("../config/config.php");
+
+class Pack {
+
+    function getAll() {
+        global $pdo;
+        return $pdo->query("SELECT * FROM `pack`");
+    }
+
+    function add($nom, $prix, $duree, $desc, $nb, $support) {
+        global $pdo;
+
+        $sql = "INSERT INTO `pack`
+        (`nom-pack`, `prix`, `duree`, `description`, `nb-proj-max`, `support-prioritaire`)
+        VALUES (?, ?, ?, ?, ?, ?)";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$nom, $prix, $duree, $desc, $nb, $support]);
+    }
+
+    function delete($id) {
+        global $pdo;
+        $stmt = $pdo->prepare("DELETE FROM `pack` WHERE `id-pack`=?");
+        $stmt->execute([$id]);
+    }
+}
+?>

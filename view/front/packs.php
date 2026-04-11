d2a5d271c9da957e66b0d911da1ae9dbe09d8262
mariemhammami1A27 <mariem.hammami@esprit.tn>
2026-04-11 11:04:55 +0100

    add front office packs UI

---
+++ view/front/packs.php
@@ -0,0 +1,19 @@
+﻿<?php
+require_once("../../model/Pack.php");
+$pack = new Pack();
+$packs = $pack->getAll();
+?>
+
+<h2>Packs</h2>
+
+<?php foreach($packs as $p): ?>
+<div>
+<h3><?= $p['nom-pack'] ?></h3>
+<p><?= $p['prix'] ?></p>
+
+<form action="../../controller/AbonnementController.php" method="POST">
+<input type="hidden" name="pack_id" value="<?= $p['id-pack'] ?>">
+<button name="subscribe">S'abonner</button>
+</form>
+</div>
+<?php endforeach; ?>
